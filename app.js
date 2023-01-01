bddecd326c5268d705a48c034aa8a4fc078c49e4
hoangzues124 <hoangzues124@gmail.com>
2023-01-01 17:32:23 +0700

    off

---
+++ app.js
@@ -0,0 +1,15 @@
+// var h2 = document.querySelector('.card h2')
+
+// window.onscroll = function(){
+//     var rectH1 = h2.getClientRects()
+//     var heightScreen = window.innerHeight
+
+
+//     if(!(rectH1.bottom < 0 || rectH1.top > heightScreen)){
+
+//         h2.classList.add('start')
+//     }else{
+//         h2.classList.remove('start')
+//     }
+// }
+
